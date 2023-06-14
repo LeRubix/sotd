2d65891bd146020ec9bb234472e347f2ec6b8a6a
LeRubix <60108068+LeRubix@users.noreply.github.com>
2023-06-15 00:21:05 +0100

    full sotd command

---
+++ sotd.py
@@ -0,0 +1,79 @@
+import discord
+import discord.utils
+import asyncio
+import datetime
+from datetime import datetime
+from discord.ext import commands
+from discord import Spotify
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+
+
+BOT_TOKEN = ""
+CHANNEL_ID = ""
+
+
+
+@client.command()
+@commands.cooldown(1,43200,commands.BucketType.user)
+async def sotd(ctx, link=None):
+    sotdchannel = ctx.guild.get_channel(CHANNEL_ID)
+    user = ctx.author
+    if(link != None):
+            msg2 = await sotdchannel.send(datetime.now().strftime(f"__(%a, %d/%m)__\n**{ctx.author.name}**'s Song of the Day:\n{link}"))
+            await ctx.message.delete()
+            await msg2.add_reaction('<:Upvote:802500869773590548>')
+            await msg2.add_reaction('<:Downvote:802500955521810432>')
+            return
+    for activity in user.activities:
+        if isinstance(activity, Spotify):
+            em = discord.Embed(color=activity.color)
+            em.title = (datetime.now().strftime(f"(%a, %d/%m)\n__{ctx.author.name}'s__ Song of the Day:"))
+            em.set_image(url=activity.album_cover_url)
+            em.add_field(name="**Song:**", value=f"{activity.title}", inline=False)
+            em.add_field(name="**Artist:**", value=f"{activity.artist}", inline=False)
+            em.add_field(name="**Album:**", value=activity.album, inline=False)
+            em.add_field(name=f"**_ _**", value=f"[Song Link](https://open.spotify.com/track/{activity.track_id})")
+            confirm_msg = await ctx.reply(f'Are you sure you want to submit **"{activity.title} - {activity.artist}"** as your song of the day?\nRespond with "yes"/"y" to confirm or "no"/"n" to cancel. Confirmation will time out in 30 seconds.')
+
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel
+
+            try:
+                response = await client.wait_for('message', check=check, timeout=30.0)
+            except asyncio.TimeoutError:
+                timeoutmsg = await ctx.send("Confirmation timed out.")
+                await ctx.message.delete()
+                await asyncio.sleep(6)
+                await timeoutmsg.delete()
+                await ctx.command.reset_cooldown(ctx)
+                return
+            
+            # if response is different than yes / y - return
+            if response.content.lower() not in ("yes", "y"):
+                await confirm_msg.delete()
+                await response.delete()
+                await ctx.message.delete()
+                ctx.command.reset_cooldown(ctx)
+                cancelmsg = await ctx.send("Cancelled Confirmation.")
+                await asyncio.sleep(6)
+                await cancelmsg.delete()
+                return
+
+            msg = await sotdchannel.send(embed=em)
+            await confirm_msg.delete()
+            await response.delete()
+            await ctx.message.delete()
+            await msg.add_reaction('👍')
+            await msg.add_reaction('👎')
+            break
+    else:
+        if(link == None):
+            errormsg = await ctx.reply("You're not listening to spotify right now. If you are, it's either a local file or discord isn't picking it up. Double check and try again or use a link instead. (?sotd <link>)")
+            await ctx.command.reset_cooldown(ctx)
+            await asyncio.sleep(6)
+            await ctx.message.delete()
+            await errormsg.delete()
+
+client.run("BOT_TOKEN")
